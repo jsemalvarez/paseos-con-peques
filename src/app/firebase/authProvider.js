0acd5006052ac4a633d46a2be496b2b3104b0550
jsemalvarez <jsemalvarez@gmail.com>
2025-03-30 11:17:13 -0300

    feat: auth with email and password

---
+++ src/app/firebase/authProvider.js
@@ -0,0 +1,59 @@
+import { signInWithEmailAndPassword, onAuthStateChanged, getAuth } from 'firebase/auth';
+import { FirebaseAuth } from './firebase';
+
+
+export const loginWithEmailPassword = async({ email, password }) => {
+    try {
+        const resp = await signInWithEmailAndPassword( FirebaseAuth, email, password );
+        const { uid, photoURL, displayName } = resp.user;
+
+        return {
+            ok: true,
+            uid, 
+            photoURL, 
+            displayName
+        }
+    } catch (error) {
+        return { ok: false, errorMessage: error.message }
+    }
+}
+
+export const logout = async () => {
+    try {
+        await FirebaseAuth.signOut();
+
+        return {
+            ok: true,
+        }
+    } catch (error) {
+        return { ok: false, errorMessage: error.message }
+    }
+}
+
+export const onCheckAuth = async () => {
+    try {
+        const user = await onCheckAuthAdapter();
+        if (!user) throw new Error("User not logged");
+
+        const { uid, email, displayName, photoURL } = user;
+        
+        return {
+            ok: true,
+            uid, 
+            email, 
+            displayName, 
+            photoURL
+        }
+    } catch (error) {
+        return { ok: false, errorMessage: error.message }
+    }
+}
+
+const onCheckAuthAdapter = () => {
+    return new Promise((resolve) => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            resolve(user || null);
+        });
+    });
+};
--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from '../layout/Layout'
 import { InputForm } from '../components/InputForm'
 import { useForm } from '../../common/hooks/useForm'
+import { loginWithEmailPassword } from '../../../app/firebase/authProvider'
 
 const initialForm  = {
   email:'email@gmail.com',
@@ -12,51 +13,73 @@ export const LoginPage = () => {
 
   const { email, password, onInputChange, onResetForm } = useForm(initialForm)
 
+  const [isLoading, setIsLoading] = useState(false)
+
+  
+  const startLogin = async(email, password) => {
+    try {
+      setIsLoading(true)
+      const user = await loginWithEmailPassword(email, password);
+      onResetForm()
+      setIsLoading(false)
+      console.log(user)
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error)
+    }
+  }
+
   const handleLogin = (e) => {
     e.preventDefault()
-    //TODO: quitar estos console
-    console.log({email})
-    console.log({password})
-    onResetForm()
+    startLogin(email, password)
   }
 
   return (
-    <Layout>
-        <div className='min-h-screen flex justify-center items-center'>
-
-          <form 
-            className='min-w-xs px-4 py-16 border-2 border-secondary rounded-xl'
-            onSubmit={ handleLogin}
-          >
-
-            <h3 className='text-xl font-bold text-secondary tracking-wide'>
-              Iniciar Sesión en PcP
-            </h3>
-
-            <InputForm 
-              title='email:'
-              name='email'
-              type='text'
-              value={email}
-              onChange={onInputChange}
-            />
-
-            <InputForm 
-              title='contraseña:'
-              name='password'
-              type='password'
-              value={password}
-              onChange={onInputChange}
-            />
-
-            <button
-              className='mt-5 w-full border-2 hover:border-secondary border-indigo-100  p-2 rounded-full hover:bg-secondary hover:text-primary text-indigo-100 tracking-wide font-semibold text-lg cursor-pointer'
-              type='submit'
-            >Iniciar Sesión</button>
-
-          </form>
-
-        </div>
-    </Layout>
+    <>
+      {
+      isLoading
+      ? (<h2>cargando...</h2>)
+      :(
+        <Layout>
+            <div className='min-h-screen flex justify-center items-center'>
+    
+              <form 
+                className='min-w-xs px-4 py-16 border-2 border-secondary rounded-xl'
+                onSubmit={ handleLogin}
+              >
+    
+                <h3 className='text-xl font-bold text-secondary tracking-wide'>
+                  Iniciar Sesión en PcP
+                </h3>
+    
+                <InputForm 
+                  title='email:'
+                  name='email'
+                  type='text'
+                  value={email}
+                  onChange={onInputChange}
+                />
+    
+                <InputForm 
+                  title='contraseña:'
+                  name='password'
+                  type='password'
+                  value={password}
+                  onChange={onInputChange}
+                />
+    
+                <button
+                  className='mt-5 w-full border-2 hover:border-secondary border-indigo-100  p-2 rounded-full hover:bg-secondary hover:text-primary text-indigo-100 tracking-wide font-semibold text-lg cursor-pointer'
+                  type='submit'
+                >Iniciar Sesión</button>
+    
+              </form>
+    
+            </div>
+        </Layout>
+      )
+    }
+    </>
+    
   )
 }
