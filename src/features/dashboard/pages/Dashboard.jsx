734954bba80fe0b6f6b180af3938d6e54cca059a
jsemalvarez <jsemalvarez@gmail.com>
2025-03-30 13:49:48 -0300

    feat: basic view about dashboard

---
+++ src/features/dashboard/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from '../layout/Layout'
+
+export const Dashboard = () => {
+  return (
+    <Layout>
+        <h3>Dashboard</h3>
+    </Layout>
+  )
+}
