f9af3dd34a7104936f4e85070e0f2cdf5f89bae8
jsemalvarez <jsemalvarez@gmail.com>
2025-03-30 13:55:01 -0300

    chore: add some comments

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -1,31 +1,30 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-
 import { PrivateRoutes } from './PrivateRoutes';
 import { PublicRoutes } from './PublicRoutes';
 
 
-
 export const AppRoutes = () => {
 
-  const status = 'not-authenticated';
+    //TODO: get user status from general state
+    const userStatus = 'not-authenticated';
 
-  const isAuthenticated = status == 'authenticated';
+    const isAuthenticated = userStatus == 'authenticated';
 
-  return (
-    <Routes>
+    return (
+        <Routes>
 
-        {
-          (isAuthenticated)
-           ? <Route path="/*" element={ <PrivateRoutes /> } />
-           : <Route path="/public/*" element={ <PublicRoutes /> } />
-        }
-        {/* Si accede a una ruta desconocida, lo redirigimos */}
-        <Route 
-            path='/*' 
-            element={ <Navigate to={isAuthenticated ? "/" : "/public"} /> } 
-        /> 
+            {
+                (isAuthenticated)
+                ? <Route path="/*" element={ <PrivateRoutes /> } />
+                : <Route path="/public/*" element={ <PublicRoutes /> } />
+            }
+            {/* Si accede a una ruta desconocida, lo redirigimos */}
+            <Route 
+                path='/*' 
+                element={ <Navigate to={isAuthenticated ? "/" : "/public"} /> } 
+            /> 
 
-    </Routes>
-  );
+        </Routes>
+    );
 }
