5220807361521e38f1de7b35fc37ba5d617bb215
jsemalvarez <jsemalvarez@gmail.com>
2025-04-01 15:12:22 -0300

    feat: basic PlacesPage

---
+++ src/features/places/page/PlacesPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PrivateLoyout } from '../../common/layouts/PrivateLoyout'
+
+export const PlacesPage = () => {
+  return (
+    <PrivateLoyout>
+        <h3 className='h-full flex justify-center items-center'>PlacesPage</h3>
+    </PrivateLoyout>
+  )
+}
