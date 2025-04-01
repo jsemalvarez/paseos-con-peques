0134e703e11643e0efe939c80087461f2ca8907d
jsemalvarez <jsemalvarez@gmail.com>
2025-04-01 15:13:37 -0300

    feat: basic EventsPage

---
+++ src/features/events/pages/EventsPage.jsx
@@ -0,0 +1,9 @@
+import { PrivateLoyout } from '../../common/layouts/PrivateLoyout'
+
+export const EventsPage = () => {
+  return (
+    <PrivateLoyout>
+        <h3 className='h-full flex justify-center items-center'>EnvetsPage</h3>
+    </PrivateLoyout>
+  )
+}
