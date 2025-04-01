3d5e25ad2e167518ee4e34b4ef31e3905ac54736
jsemalvarez <jsemalvarez@gmail.com>
2025-04-01 15:17:06 -0300

    feat: add PlacesPage and EventsPage to the routes

--- src/app/routes/PrivateRoutes.jsx
+++ src/app/routes/PrivateRoutes.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard } from "../../features/dashboard/pages/Dashboard";
+import { PlacesPage } from "../../features/places/page/PlacesPage";
+import { EventsPage } from "../../features/events/pages/EventsPage";
 
 
 export const PrivateRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/places" element={<PlacesPage />} />
+      <Route path="/events" element={<EventsPage />} />
       {/* <Route path="/profile" element={<Profile />} /> */}
       {/* Si accede a una ruta no definida en privado, lo llevamos al dashboard */}
       <Route path="*" element={<Navigate to="/" />} />
