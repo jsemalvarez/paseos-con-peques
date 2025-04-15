5ae4ce664db3d999f6c04bac23947adcc33786d1
jsemalvarez <jsemalvarez@gmail.com>
2025-04-15 20:18:19 -0300

    feat: add Loader.jsx

---
+++ src/features/common/components/Loader.jsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+    return (
+      <div className="min-h-screen flex justify-center items-center w-full h-full p-4 bg-primary">
+        <div className="w-12 h-12 border-4 border-secondary border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+};
