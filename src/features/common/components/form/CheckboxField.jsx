026d9f6b52473ea3e9bc27fdffe6e81b070e62aa
jsemalvarez <jsemalvarez@gmail.com>
2025-05-01 21:55:05 -0300

    feat: CheckboxField.jsx

---
+++ src/features/common/components/form/CheckboxField.jsx
@@ -0,0 +1,17 @@
+
+export const CheckboxField = ({ label, name, checked, onChange, className = '' }) => {
+    return (
+        <div className={`pt-2 ${className}`}>
+            <label className="flex items-center gap-2 cursor-pointer">
+            <input 
+                type="checkbox" 
+                name={name}
+                checked={checked}
+                onChange={onChange}
+                className="accent-indigo-600 w-4 h-4"
+            />
+            {label}
+            </label>
+        </div>
+    );
+};
