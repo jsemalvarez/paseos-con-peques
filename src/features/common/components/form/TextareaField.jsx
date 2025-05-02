9be64ff796a74c444cb54cbaa33b8328e0818de8
jsemalvarez <jsemalvarez@gmail.com>
2025-05-01 21:58:41 -0300

    feat: TextareaField.jsx

---
+++ src/features/common/components/form/TextareaField.jsx
@@ -0,0 +1,27 @@
+
+
+export const TextareaField = ({
+    name,
+    value,
+    onChange,
+    rows = 6,
+    placeholder = '',
+    label,
+    className = '',
+}) => {
+    return (
+        <div className="mt-4">
+            {label && <label htmlFor={name} className="block mb-1 font-medium">{label}</label>}
+            <textarea
+            id={name}
+            name={name}
+            value={value}
+            onChange={onChange}
+            rows={rows}
+            placeholder={placeholder}
+            className={`w-full border border-gray-400 hover:border-secondary focus:border-secondary p-2 rounded-xl focus:bg-secondary focus:outline-none ${className}`}
+            />
+        </div>
+    );
+};
+  
