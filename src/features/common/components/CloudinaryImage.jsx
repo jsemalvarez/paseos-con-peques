31b5ec876e507ad8af639efa093cd6cf91257ca1
jsemalvarez <jsemalvarez@gmail.com>
2025-06-14 18:06:40 -0300

    style: add styles to default fallback image in CloudinaryImage

--- src/features/common/components/CloudinaryImage.jsx
+++ src/features/common/components/CloudinaryImage.jsx
@@ -11,6 +11,7 @@ export const CloudinaryImage = ({ publicId, alt = "", className = "" }) => {
             <img 
                 src={ fallbackImage }
                 alt="logo pcp"
+                className={className}
             />
         )
     }
