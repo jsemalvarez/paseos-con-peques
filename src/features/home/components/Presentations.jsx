9ae9861a46e5f34a2a4b8fc6d2078d95400b392e
jsemalvarez <jsemalvarez@gmail.com>
2025-06-17 20:01:13 -0300

    style: update styles for install app message

--- src/features/home/components/Presentations.jsx
+++ src/features/home/components/Presentations.jsx
@@ -74,8 +74,11 @@ export const Presentations = () => {
 
       {
         (deferredPrompt && isMobile) && (
-          <div className="flex flex-col items-center mt-4">
-            <div className="bg-blue-100 border border-blue-300 text-blue-800 px-4 py-3 rounded text-center mt-4 text-sm">
+          <></>
+        )
+      }
+          <div className="flex flex-col items-center mx-6 mt-4">
+            <div className="bg-blue-100 border border-blue-300 text-blue-800 px-4 py-3 rounded text-center text-sm">
               <p className="font-medium">
                 ¡Instalá nuestra app para tener acceso más rápido desde tu pantalla de inicio!
               </p>
@@ -88,8 +91,6 @@ export const Presentations = () => {
               Instalar App
             </button>
           </div>
-        )
-      }
 
 
     </div>
