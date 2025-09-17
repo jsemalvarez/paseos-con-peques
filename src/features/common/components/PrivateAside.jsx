5f73826f0ce84cb353ad6f9b48fae1f8f0e435a8
jsemalvarez <jsemalvarez@gmail.com>
2025-09-17 20:21:16 -0300

    refactor: change bg color and remuve PrivateNavbar in PrivateLayout

--- src/features/common/components/PrivateAside.jsx
+++ src/features/common/components/PrivateAside.jsx
@@ -1,8 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import { XMarkIcon } from './Icons';
+import { useUserLogin } from '../../auth/hooks/useUserLogin';
 
 
 export const PrivateAside = ({isOpen, setIsOpen }) => {
+
+    const { user, logout } = useUserLogin()
+
+    const handleLogOut = () => {
+        logout()
+    }
 
     const itemClass = "w-full border-l-6 border-secondary mb-2 p-1 cursor-pointer flex justify-center hover:bg-secondary hover:text-primary hover:border-secondary";
     const activeItemClass = "bg-secondary text-primary border-l-6 border-secondary";
@@ -10,19 +17,23 @@ export const PrivateAside = ({isOpen, setIsOpen }) => {
     return (
         <aside
             className={`
-                fixed top-0 left-0 md:relative h-screen w-[300px] border-r-2 border-secondary p-4 bg-primary
+                bg-gradient-to-br from-secondary to-pink-300
+                fixed top-0 left-0  md:relative h-screen w-[300px] border-r-1 border-white flex flex-col p-4 bg-secondary
                 transform transition-transform duration-300 ease-in-out
                 ${isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}
             `}
         >
-            <div
-                className='block md:hidden cursor-pointer text-gray-600 hover:text-red-500 text-lg font-bold mb-2 flex flex-row-reverse' 
-                onClick={ () => setIsOpen(false) }
-            >
-                <XMarkIcon style='transition-all duration-300 hover:text-gray-200' />
+            <div className='mb-4'>
+                <div
+                    className='block md:hidden cursor-pointer mb-2 flex flex-row-reverse' 
+                    onClick={ () => setIsOpen(false) }
+                >
+                    <XMarkIcon style='transition-all duration-300 text-primary hover:text-secondary text-lg font-bold' />
+                </div>
+                <p className='text-primary font-bold text-center'>{user.email}</p>
             </div>
-            <ul className="flex flex-col items-center">
-                <li className="w-9/10">
+            <ul className="grow bg-gradient-to-br from-primary-light to-primary p-2 mb-4">
+                <li>
                     <NavLink 
                         to='/'
                         className={({ isActive }) => `${itemClass} ${isActive ? activeItemClass : undefined}`}
@@ -30,7 +41,7 @@ export const PrivateAside = ({isOpen, setIsOpen }) => {
                         Panel
                     </NavLink>
                 </li>
-                <li className="w-9/10">
+                <li>
                     <NavLink 
                         to='/places'
                         className={({ isActive }) => `${itemClass} ${isActive ? activeItemClass : undefined}`}
@@ -38,7 +49,7 @@ export const PrivateAside = ({isOpen, setIsOpen }) => {
                         Lugares
                     </NavLink>
                 </li>
-                <li className="w-9/10">
+                <li>
                     <NavLink 
                         to='/events'
                         className={({ isActive }) => `${itemClass} ${isActive ? activeItemClass : undefined}`}
@@ -47,6 +58,12 @@ export const PrivateAside = ({isOpen, setIsOpen }) => {
                     </NavLink>
                 </li>
             </ul>
+            <button
+                className='bg-secondary p-1 text-white font-bold rounded-sm cursor-pointer hover:bg-primary'
+                onClick={ handleLogOut }
+            >
+                Salir
+            </button>
         </aside>
     )
 }
--- src/features/common/layouts/PrivateLoyout.jsx
+++ src/features/common/layouts/PrivateLoyout.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { PrivateAside } from "../components/PrivateAside"
 import { PrivateNavbar } from "../components/PrivateNavbar"
+import { MenuIcon } from "../components/Icons";
 
 
 
@@ -9,7 +10,7 @@ export const PrivateLoyout = ({children}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='h-screen w-full flex bg-primary text-indigo-100'>
+    <div className='h-screen w-full flex bg-gradient-to-bl from-primary via-primary-light to-primary text-indigo-100'>
 
       <PrivateAside
         isOpen={ isOpen }
@@ -19,9 +20,17 @@ export const PrivateLoyout = ({children}) => {
       {/* Contenedor principal scrolleable */}
       <div className="flex-1">
 
-        <PrivateNavbar setIsOpen={setIsOpen} />
+        {/* <PrivateNavbar setIsOpen={setIsOpen} /> */}
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(true)} 
+            className="bg-secondary text-white p-3 cursor-pointer hover:bg-rose-300 transition-all duration-300"
+            >
+            <MenuIcon style='transition-all duration-300 text-primary' />
+          </button>
+        </div>
 
-        <div className="h-[calc(100vh-60px)] pt-4 overflow-y-auto">
+        <div className="h-screen md:pt-4 overflow-y-auto">
           {children}
         </div>
 
