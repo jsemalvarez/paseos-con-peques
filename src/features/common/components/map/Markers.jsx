1339e76b8bb554a78c65a0f5258ccfd3ca10bdab
jsemalvarez <jsemalvarez@gmail.com>
2025-10-04 12:33:38 -0300

    docs: add some comments for future improvement

--- src/features/common/components/map/Markers.jsx
+++ src/features/common/components/map/Markers.jsx
@@ -1,7 +1,6 @@
 import { Marker } from 'react-leaflet'
 
 //TODO: pensar nombres que dejen claro que esto pertenece a place
-// import { createSvgIcon } from '../../../places/utils/createSvgIcon'
 import { ICONS_BY_TYPE } from '../../../places/utils/iconsByType'
 import { COLORS_BY_CATEGORIES } from '../../../places/utils/categories'
 import { getCustomSvgIcon } from '../../../places/utils/getCustomSvgIcon'
@@ -32,6 +31,7 @@ export const Markers = ({ places, handleClick }) => {
               //TODO: guardar el nombre del color en la DB, y no el color en si
               const bgColor = place.bgColor || COLORS_BY_CATEGORIES.ENTERTIME; 
               //TODO: createSvgIcon tiene que estar en este directorio
+              //TODO: place.photoUrl tiene que ser place.photoId
               icon = (place.hasCustomIcon)
                 ?getCustomSvgIcon({imageId: place.photoUrl})
                 :createSvgIcon({ bgColor, svgIconType })
